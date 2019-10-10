a17ae4f794a4f8d374f8e675eff4225b01b10e1c
iamgeniuswei <iamgeniuswei@sina.com>
2019-10-10 15:55:06 +0800

    1)threeSumClosest 2)fourSum

--- Solution.h
+++ Solution.h
@@ -7,15 +7,152 @@
 
 #include <map>
 #include <vector>
+#include <algorithm>
+#include <assert.h>
+#include <cmath>
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum_1(vector<int>& nums) {
+        int size = nums.size();
+        vector<vector<int>> ret;
+        map<int, int> assist;
+        if(size < 3)
+            return ret;
+        sort(nums.begin(), nums.end());
+        for(int k=0; k<size-2; )
+        {
+            int i=k+1, j=size-1;
+            while (i<j)
+            {
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum < 0)
+                {
+                    while (i<j)
+                    {
+                        if(nums[i] == nums[i+1])
+                            i++;
+                        else
+                        {
+                            i+=1;
+                            break;
+                        }
+                    }
+                } else if(sum>0)
+                {
+                    while (i<j)
+                    {
+                        if(nums[j] == nums[j-1])
+                            j--;
+                        else
+                        {
+                            j-=1;
+                            break;
+                        }
+                    }
+                } else{
+                    vector<int> candi{nums[k],
+                                      nums[i],
+                                      nums[j]};
+                    ret.push_back(candi);
+                    while (i<j)
+                    {
+                        if(nums[i] == nums[i+1])
+                            i++;
+                        else
+                        {
+                            i+=1;
+                            break;
+                        }
+                    }
+                }
+            }
+            while (k<size-2)
+            {
+                if(nums[k] == nums[k+1])
+                    k++;
+                else
+                {
+                    k+=1;
+                    break;
+                }
+            }
+        }
 
-
-
-
+        return ret;
     }
+
+
+    int threeSumClosest(vector<int>& nums, int target) {
+        int size = nums.size();
+        assert(size >2);
+        sort(nums.begin(), nums.end());
+        int sum = nums[0] + nums[1] + nums[2];
+        int near = abs(sum-target);
+        for(int k =0; k<size;k++)
+        {
+            int i=k+1, j=size-1;
+            while (i<j)
+            {
+                int newsum = nums[k] + nums[i] + nums[j];
+                if(abs(target-newsum) < abs(target-sum))
+                    sum = newsum;
+                if(target > newsum)
+                    i++;
+                else if(target < newsum)
+                    j--;
+                else
+                    return sum;
+            }
+        }
+        return sum;
+    }
+
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ret;
+        int size = nums.size();
+        if(size <4)
+            return ret;
+        sort(nums.begin(), nums.end());
+        int a = 0, b=0, c=0, d=0;
+        int sum = 0;
+        while (a < (size - 3))
+        {
+            b = a+1, c = size-2, d = size - 1;
+            while (b < c)
+            {
+                sum = nums[a] + nums[b] + nums[c] + nums[d];
+                if(sum == target)
+                {
+                    vector<int> candi{nums[a], nums[b], nums[c], nums[d]};
+                    ret.push_back(candi);
+                    while (nums[c] == nums[--c] && c > b);
+                    while (nums[b] == nums[++b] && c > b);
+                } else if(sum < target)
+                {
+                    while (nums[b] == nums[++b] && b < c);
+                } else if(sum > target)
+                {
+                    while (nums[c] == nums[--c] && c > b);
+                }
+                if(b>=c)
+                {
+                    if (d>c)
+                    {
+                        while (nums[d] == nums[--d] && d>(a+2));
+                        if(d <= a+2)
+                            break;
+                        c=d-1;
+                        b = a+1;
+                    }
+                }
+            }
+
+            while (nums[a]==nums[++a] && a<(size-3));
+        }
+        return ret;
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,89 @@
 #include <iostream>
 #include <gtest/gtest.h>
+using namespace std;
+
+#include "Solution.h"
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+
+TEST(testSolution, test_threeSum)
+{
+    Solution solution;
+    vector<int> nums{-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ret = solution.threeSum_1(nums);
+    EXPECT_EQ(ret.size(), 2);
+}
+
+TEST(testSolution, test_threeSum2)
+{
+    Solution solution;
+    vector<int> nums{0, 0, 0, 0};
+    vector<vector<int>> ret = solution.threeSum_1(nums);
+    EXPECT_EQ(ret.size(), 1);
+}
+
+TEST(testSolution, test_threeSumClosest)
+{
+    Solution solution;
+    vector<int> nums{-1, 2, 1, -4};
+    int ret = solution.threeSumClosest(nums, 1);
+    EXPECT_EQ(ret, 2);
+}
+TEST(testSolution, test_threeSumClosest2)
+{
+    Solution solution;
+    vector<int> nums{1, 1, 1, 0};
+    int ret = solution.threeSumClosest(nums, -100);
+    EXPECT_EQ(ret, 2);
+}
+
+TEST(testSolution, test_threeSumClosest3)
+{
+    Solution solution;
+    vector<int> nums{1,1,-1,-1,3};
+    int ret = solution.threeSumClosest(nums, -1);
+    EXPECT_EQ(ret, -1);
+}
+
+TEST(testSolution, test_threeSumClosest4)
+{
+    Solution solution;
+    vector<int> nums{1,1,-1,-1,3};
+    int ret = solution.threeSumClosest(nums, 3);
+    EXPECT_EQ(ret, 3);
+}
+
+TEST(testSolution, test_threeSumClosest5)
+{
+    Solution solution;
+    vector<int> nums{1,2,4,8,16,32,64,128};
+    int ret = solution.threeSumClosest(nums, 82);
+    EXPECT_EQ(ret, 82);
+}
+
+TEST(testSolution, test_fourSum)
+{
+    Solution solution;
+    vector<int> nums{1, 0, -1, 0, -2, 2};
+    vector<vector<int>> ret = solution.fourSum(nums, 0);
+    EXPECT_EQ(ret.size() , 3);
+}
+
+TEST(testSolution, test_fourSum1)
+{
+    Solution solution;
+    vector<int> nums{-3, -1, 0, 2, 4, 5};
+    vector<vector<int>> ret = solution.fourSum(nums, 2);
+    EXPECT_EQ(ret.size() , 1);
+}
+
+
+TEST(testSolution, test_fourSum2)
+{
+    Solution solution;
+    vector<int> nums{-1,-5,-5,-3,2,5,0,4};
+    vector<vector<int>> ret = solution.fourSum(nums, -7);
+    EXPECT_EQ(ret.size() , 2);
 }
